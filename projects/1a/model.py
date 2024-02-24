c431d44f46b55530b20cb6aaa227e59b0b89c424
tARTar78 <143780971+tARTar78@users.noreply.github.com>
2024-02-24 19:31:48 +0300

    Update model.py

--- projects/1a/model.py
+++ projects/1a/model.py
@@ -8,21 +8,21 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 #
 # Dataset fields
 #
-fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
-num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)]
+fields = ["id", "label"] + numeric_features + categorical_features
+
 
 #
 # Model pipeline
 #
 
 # We create the preprocessing pipelines for both numeric and categorical data.
-numeric_features = ['CLEANLINESS', 'ROOM', 'SERVICE', 'LOCATION']
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
 #    ('scaler', StandardScaler())
 ])
 
-categorical_features = ['city', 'country']
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
     ('onehot', OneHotEncoder(handle_unknown='ignore'))
