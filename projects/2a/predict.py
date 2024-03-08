288a19046104d5d6b5d13889488cd3adcfc6b25d
Шейнов_Артём <tARTar78@edge1.ru-central1.internal>
2024-03-08 19:24:14 +0000

    pl

--- projects/2a/predict.py
+++ projects/2a/predict.py
@@ -27,7 +27,7 @@ read_opts=dict(
 )
 
 for df in pd.read_csv(sys.stdin, **read_opts):
-    df = df.replace('\\N' , np.nan)
+    df = df.replace({'\\N' : np.nan, "" : np.nan})
     pred = model.predict(df)
     out = zip(df.id, pred)
     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
