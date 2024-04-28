365c833279f218493b92f2e8f9c63bda2a3416ba
Шейнов_Артём <tARTar78@edge1.ru-central1.internal>
2024-04-28 16:09:13 +0000

    6k

--- projects/6/tARTar78_dag.py
+++ projects/6/tARTar78_dag.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 spark_binary1 = '/usr/bin/spark3-submit'
 
-withd DAG(dag_id='tARTar78_dag', start_date=datetime(2024, 5, 27), schedule_interval=None, catchup=False) as dag:
+with DAG(dag_id='tARTar78_dag', start_date=datetime(2024, 5, 27), schedule_interval=None, catchup=False) as dag:
     base_dir = '{{ dag.conf["base_dir"] if dag else "" }}'
 
     feature_eng_train_task = SparkSubmitOperator(
