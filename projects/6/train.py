b6f6db1c4a60860898f044ef904947f4c15046d4
Шейнов_Артём <tARTar78@edge1.ru-central1.internal>
2024-04-29 13:41:30 +0000

    45

--- projects/6/train.py
+++ projects/6/train.py
@@ -1,8 +1,9 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import joblib
 import pandas as pd
 import sys
-
 
 train_in = sys.argv[2]
 sklearn_model_out = sys.argv[4]
@@ -10,10 +11,21 @@ sklearn_model_out = sys.argv[4]
 train_data = pd.read_parquet(train_in)
 X_train = train_data.drop('label', axis=1)
 y_train = train_data['label']
-# Обучение логистической регрессии
-classifier = LogisticRegression()
+
+# Define the preprocessing steps
+preprocessor = Pipeline([
+    ('categorical', OneHotEncoder()),
+    ('numeric', StandardScaler())
+])
+
+# Define the full pipeline
+classifier = Pipeline([
+    ('preprocess', preprocessor),
+    ('model', LogisticRegression())
+])
+
+# Fit the pipeline to the data
 classifier.fit(X_train, y_train)
-# Сохранение обученной модели
+
+# Save the trained model
 joblib.dump(classifier, sklearn_model_out)
-
-
