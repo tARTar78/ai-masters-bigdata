99d70fd7c982a9b620566880c73fd8491ce5f8b2
Шейнов_Артём <tARTar78@edge1.ru-central1.internal>
2024-04-29 15:02:29 +0000

    333

--- projects/6/inference.py
+++ projects/6/inference.py
@@ -6,6 +6,7 @@ spark.sparkContext.setLogLevel('WARN')
 import joblib
 import sys
 from pyspark.sql.functions import pandas_udf, PandasUDFType
+from pyspark.sql.types import FloatType
 
 test_in = sys.argv[2]
 pred_out = sys.argv[4]
